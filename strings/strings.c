8723b585a6e3fa5857c66ac4ae35164cd3b0c0b3
axreldable <starikovaleksei1992@mail.ru>
2018-12-22 14:04:43 +0300

    fix strings without args

--- strings/strings.c
+++ strings/strings.c
@@ -58,13 +58,61 @@ int strings(char *name, size_t start_size, int length) {
     return 0;
 }
 
-int main(int argc, char **argv) {
-    if (argc < 2) {
-        fprintf(stderr, "USAGE: ./a.out file_list\n");
-        exit(1);
+int strings_std(size_t start_size, int length) {
+    char *str;
+    size_t len = 0;
+    str = realloc(NULL, sizeof(char) * start_size);
+    if (!str) {
+        perror("not enough memory");
+    }
+    int ch;
+    while ((ch = getchar()) != EOF) {
+        if (ch == '\t' || (ch >= 32 && ch < 127)) {
+//            printf("!%d", ch);
+            str[len++] = ch;
+            if (len == start_size) {
+                str = realloc(str, sizeof(char) * (start_size += 16));
+                if (!str) {
+                    perror("not enough memory");
+                }
+            }
+            if (ch == '\n') {
+                str[len++] = '\0';
+                if (len >= length + 2) {
+                    printf("%s", str);
+                }
+                len = 0;
+                str = realloc(NULL, sizeof(char) * start_size);
+                if (!str) {
+                    perror("not enough memory");
+                }
+            }
+        } else {
+            if (len >= length) {
+                str[len++] = '\n';
+                str[len++] = '\0';
+                printf("%s", str);
+                len = 0;
+                str = realloc(NULL, sizeof(char) * start_size);
+                if (!str) {
+                    perror("not enough memory");
+                }
+            } else {
+                len = 0;
+            }
+        }
+
     }
 
-    for (int i = 1; i < argc; i++) {
-        strings(argv[i], 10, 4);
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        strings_std(10, 4);
+    }  else {
+        for (int i = 1; i < argc; i++) {
+            strings(argv[i], 10, 4);
+        }
     }
 }
